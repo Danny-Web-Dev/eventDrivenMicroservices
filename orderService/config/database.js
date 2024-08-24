23e03ee2ea77d9f3249f57450b8b61ec36a047e1
danny <danny.mizrachi11@gmail.com>
2024-08-24 22:59:51 +0300

    add notice

--- orderService/config/database.js
+++ orderService/config/database.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 
+// ***** make sure you insert you local mysql configs *****
+
 const sequelize = new Sequelize('order_service_db', 'root', '12345678', {
 	host: 'localhost',
 	dialect: 'mysql',
