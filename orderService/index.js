d365e1c6f63d0eae598a0957eab5567f88043f3f
danny <danny.mizrachi11@gmail.com>
2024-08-25 15:36:22 +0300

    code cleanup

--- orderService/index.js
+++ orderService/index.js
@@ -7,6 +7,11 @@ app.use(express.json());
 
 app.use('/api', orderRoutes);
 
+// Handle other routes and errors
+app.use((req, res) => {
+	res.status(404).json({ error: 'Route not found' });
+});
+
 // init sequalize and run app.
 sequelize
 	.sync()
--- userService/index.js
+++ userService/index.js
@@ -12,11 +12,6 @@ app.use((req, res) => {
 	res.status(404).json({ error: 'Route not found' });
 });
 
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({ error: 'Something went wrong!' });
-});
-
 // init sequalize and run app.
 sequelize
 	.sync()
