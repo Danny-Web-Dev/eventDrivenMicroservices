f81b17abfd29081b2474f472b06e56692bcfd1bf
danny <danny.mizrachi11@gmail.com>
2024-08-25 07:36:15 +0300

    code cleanup

--- orderService/config/database.js
+++ orderService/config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize('order_service_db', 'root', '12345678', {
 	dialectOptions: {
 		timeZone: 'Etc/GMT-3', // Israel Time is GMT+3
 	},
-	timezone: '+03:00', // For Sequelize to handle date strings
+	timezone: '+03:00',
 });
 
 module.exports = sequelize;
