cb591eb125121e953e67a3f7c394b89f34299d2b
danny <danny.mizrachi11@gmail.com>
2024-08-25 07:39:22 +0300

    remove mysql gmt

--- userService/config/database.js
+++ userService/config/database.js
@@ -5,9 +5,6 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('user_service_db', 'root', '12345678', {
 	host: 'localhost',
 	dialect: 'mysql',
-	dialectOptions: {
-		timeZone: 'Etc/GMT-3', // Israel Time is GMT+3
-	},
 	timezone: '+03:00',
 });
 
