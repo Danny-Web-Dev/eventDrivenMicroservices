5f146848315f0604d18866992f2a94e9484675fc
danny <danny.mizrachi11@gmail.com>
2024-08-25 09:03:53 +0300

    code cleanup

--- eventService/index.js
+++ eventService/index.js
@@ -1,39 +1,56 @@
 const NATS = require('nats');
 
+// color pallet for logs.
+const reset = '\x1b[0m';
+const bright = '\x1b[1m';
+const fgGreen = '\x1b[32m';
+const fgYellow = '\x1b[33m';
+
 // NATS setup
 const nats = NATS.connect({ url: 'nats://localhost:4222' });
 
+// Common log function with color
+const logEvent = (eventLabel, message, color = fgGreen) => {
+	console.log(`${bright}${color}${eventLabel}:${reset} ${fgYellow}${message}${reset}`);
+};
+
+// Common subscription function
+const subscribeToEvent = (event, callback) => {
+	nats.subscribe(event, (msg) => {
+		callback(msg);
+	});
+};
+
 // Listen to 'User Created' event
-nats.subscribe('user:created', (msg) => {
-	const data = JSON.parse(msg);
-	console.log(`User Created Event Received: ${data.name} (${data.email})`);
+subscribeToEvent('user:created', (msg) => {
+	logEvent('User Created Event Received', `User Details: ${msg}`);
 });
 
-// Listen to 'User get' event
-nats.subscribe('user:get', (msg) => {
-	console.log(`User Get Event Received: ${msg}`);
+// Listen to 'User Get' event
+subscribeToEvent('user:get', (msg) => {
+	logEvent('User Get Event Received', `User Details: ${msg}`);
 });
 
-// Listen to 'User update' event
-nats.subscribe('user:update', (msg) => {
-	console.log(`User Update Event Received: ${msg}`);
+// Listen to 'User Update' event
+subscribeToEvent('user:update', (msg) => {
+	logEvent('User Update Event Received:', `User Details: ${msg}`);
 });
 
 // Listen to 'Order Placed' event
-nats.subscribe('order:placed', (msg) => {
+subscribeToEvent('order:placed', (msg) => {
 	const data = JSON.parse(msg);
-	console.log(`Order Placed Event Received: OrderId: ${data.id} by UserId: ${data.userId}`);
+	logEvent('Order Placed Event Received', `OrderId: ${data.id} by UserId: ${data.userId}`);
 });
 
 // Listen to 'Order Get' event
-nats.subscribe('order:get', (msg) => {
-	console.log(`Order Get Event Received: Order ${msg}`);
+subscribeToEvent('order:get', (msg) => {
+	logEvent('Order Get Event Received', `Order Details: ${msg}`);
 });
 
 // Listen to 'Order Cancelled' event
-nats.subscribe('order:cancelled', (msg) => {
+subscribeToEvent('order:cancelled', (msg) => {
 	const data = JSON.parse(msg);
-	console.log(`Order Cancelled Event Received: OrderId: ${data.id}`);
+	logEvent('Order Cancelled Event Received', `OrderId: ${data.id}`);
 });
 
-console.log('Event Service listening for events...');
+console.log(`${bright}${fgGreen}Event Service listening for events...${reset}`);
