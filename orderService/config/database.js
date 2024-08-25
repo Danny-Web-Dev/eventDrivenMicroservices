53ba10c4f97cf5c42505d84c9b59fb4ebd663304
danny <danny.mizrachi11@gmail.com>
2024-08-25 10:50:44 +0300

    code cleanup

--- orderService/config/database.js
+++ orderService/config/database.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // ***** make sure you insert you local mysql configs *****
 
+// Sequelize ORM configuration
 const sequelize = new Sequelize('orders', 'root', '12345678', {
 	host: 'localhost',
 	dialect: 'mysql',
--- userService/config/database.js
+++ userService/config/database.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // ***** make sure you insert your local mysql configs *****
 
+// Sequelize ORM configuration
 const sequelize = new Sequelize('users', 'root', '12345678', {
 	host: 'localhost',
 	dialect: 'mysql',
